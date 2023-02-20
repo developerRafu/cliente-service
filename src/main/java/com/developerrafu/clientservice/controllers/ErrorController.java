7742a1ec3cdca74dbbe9883c63b333e5ff13393b
rafael <rafael.henrique@techlead.com.br>
2023-02-20 11:32:12 -0300

    formantando código com o maven.

--- src/main/java/com/developerrafu/clientservice/controllers/ErrorController.java
+++ src/main/java/com/developerrafu/clientservice/controllers/ErrorController.java
@@ -15,33 +15,58 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 @RestController
 public class ErrorController {
-    @ExceptionHandler(CepNotFoundException.class)
-    public ResponseEntity<DefaultError> handleCepNotFoundException(final CepNotFoundException ex, final WebRequest request) {
-        final var error = DefaultError.builder().message("Erro ao localizar cep: " + ex.getCep()).code(HttpStatus.NOT_FOUND.value()).build();
-        return ResponseEntity.status(error.getCode()).body(error);
-    }
+  @ExceptionHandler(CepNotFoundException.class)
+  public ResponseEntity<DefaultError> handleCepNotFoundException(
+      final CepNotFoundException ex, final WebRequest request) {
+    final var error =
+        DefaultError.builder()
+            .message("Erro ao localizar cep: " + ex.getCep())
+            .code(HttpStatus.NOT_FOUND.value())
+            .build();
+    return ResponseEntity.status(error.getCode()).body(error);
+  }
 
-    @ExceptionHandler(ClienteNotFoundException.class)
-    public ResponseEntity<DefaultError> handleClienteNotFoundException(final ClienteNotFoundException ex, final WebRequest request) {
-        final var error = DefaultError.builder().message("Erro ao localizar cliente").code(HttpStatus.NOT_FOUND.value()).build();
-        return ResponseEntity.status(error.getCode()).body(error);
-    }
+  @ExceptionHandler(ClienteNotFoundException.class)
+  public ResponseEntity<DefaultError> handleClienteNotFoundException(
+      final ClienteNotFoundException ex, final WebRequest request) {
+    final var error =
+        DefaultError.builder()
+            .message("Erro ao localizar cliente")
+            .code(HttpStatus.NOT_FOUND.value())
+            .build();
+    return ResponseEntity.status(error.getCode()).body(error);
+  }
 
-    @ExceptionHandler(ExternalRequestException.class)
-    public ResponseEntity<DefaultError> handleExternalRequestException(final ExternalRequestException ex, final WebRequest request) {
-        final var error = DefaultError.builder().message(ex.getIssue().getMessage()).code(HttpStatus.INTERNAL_SERVER_ERROR.value()).build();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
-    }
+  @ExceptionHandler(ExternalRequestException.class)
+  public ResponseEntity<DefaultError> handleExternalRequestException(
+      final ExternalRequestException ex, final WebRequest request) {
+    final var error =
+        DefaultError.builder()
+            .message(ex.getIssue().getMessage())
+            .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            .build();
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+  }
 
-    @ExceptionHandler(UnprocessedResponseException.class)
-    public ResponseEntity<DefaultError> handleUnprocessedResponseException(final UnprocessedResponseException ex, final WebRequest request) {
-        final var error = DefaultError.builder().message(ex.getMessage()).code(HttpStatus.INTERNAL_SERVER_ERROR.value()).build();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
-    }
+  @ExceptionHandler(UnprocessedResponseException.class)
+  public ResponseEntity<DefaultError> handleUnprocessedResponseException(
+      final UnprocessedResponseException ex, final WebRequest request) {
+    final var error =
+        DefaultError.builder()
+            .message(ex.getMessage())
+            .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            .build();
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+  }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<DefaultError> handleRuntimeException(final RuntimeException ex, final WebRequest request) {
-        final var error = DefaultError.builder().message("Erro interno no servidor, contate os administradores").code(HttpStatus.INTERNAL_SERVER_ERROR.value()).build();
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
-    }
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<DefaultError> handleRuntimeException(
+      final RuntimeException ex, final WebRequest request) {
+    final var error =
+        DefaultError.builder()
+            .message("Erro interno no servidor, contate os administradores")
+            .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+            .build();
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+  }
 }
